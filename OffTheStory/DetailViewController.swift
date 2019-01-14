ea6b59379ea686a6cfdf28131da4dda8b58c2e74
raon29 <raon0229@gmail.com>
2019-01-14 21:44:06 +0900

    Initial Commit

---
+++ OffTheStory/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  OffTheStory
+//
+//  Created by misong lee on 2019. 1. 14..
+//  Copyright © 2019년 misong lee. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
